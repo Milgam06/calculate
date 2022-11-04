7f8208f4c952904976ca1b6aaf975b5a01bae15f
Milgam06 <milgamfruit@gmail.com>
2022-11-04 09:12:29 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   "\352\265\254\354\213\254\354\233\220\354\213\254.py"

--- 구심원심.py
+++ 구심원심.py
@@ -1,25 +1,23 @@
 import math
 
-
-class CTP: #값 클래스 생성
-    def __init__(self, mass1, radius1, speed1):
-        self.mass1 = float(mass1)
-        self.speed1 = float(speed1)
-        self.radius1 = float(radius1)
-        print("질량 > {}kg\n속력 > {}m/s\n반지름 > {}m".format(mass1, speed1, radius1))
-        print("구심력 공식 > mv^/r")
-        result1 = math.pow((mass1 * speed1), 2) / radius1
-        print(str(result1) + " N")
+#값 클래스 생성
+def CTP(mass1, radius1, speed1):
+    speed1 = float(speed1)
+    radius1 = float(radius1)
+    print("질량 > {}kg\n속력 > {}m/s\n반지름 > {}m".format(mass1, speed1, radius1))
+    print("구심력 공식 > mv^/r")
+    result1 = math.pow((mass1 * speed1), 2) / radius1
+    print(str(result1) + " N")
         
-class CTF:
-    def __init__(self, mass2, speed2, radius2):
-        self.mass2 = float(mass2)
-        self.speed2 = float(speed2)
-        self.radius2 = float(radius2)
-        print("질량 > {}kg\n속력 > {}m/s\n반지름 > {}m".format(mass2, speed2, radius2))
-        print("원심력 공식 > -mv^/r")
-        result2 = (math.pow(-1 * (mass2 * speed2), 2)) / radius2
-        print(str(result2) + " N")
+
+def CTF(mass2, speed2, radius2):
+    mass2 = float(mass2)
+    speed2 = float(speed2)
+    radius2 = float(radius2)
+    print("질량 > {}kg\n속력 > {}m/s\n반지름 > {}m".format(mass2, speed2, radius2))
+    print("원심력 공식 > -mv^/r")
+    result2 = (math.pow(-1 * (mass2 * speed2), 2)) / radius2
+    print(str(result2) + " N")
 
 Choice = str(input("구심력 or 원심력\n> "))
 if Choice == "구심력":
